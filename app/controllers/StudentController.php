0f2baf5495928a303d4cacda2fa115a4c383428e
Baseer Ahmad Baheer <csd.ku.af@gmail.com>
2014-09-17 11:51:58 +0430

    Few more changes

--- app/controllers/StudentController.php
+++ app/controllers/StudentController.php
@@ -13,7 +13,23 @@ class StudentController extends BaseController {
       'teacher_name' => 'required',
       'teacher_last_name' => 'required'
     ));
+    if($validate ->fails()){
+      return Redirect::route('get Student')->withErrors($validate)->withInput();
+    }
+    else{
+      $student = new Students();
+      $student->MONITORING_PROVINCE = Input::get('pro');
+      $student->TTC_NAME = Input::get('ttc_name');
+      $student->TEACHER_NAME = Input::get('teacher_name');
+      $student->TEACHER_SURNAME = Input::get('teacher_last_name');
 
+      if($student->save()){
+        return Redirect::route('getStudent')->with('success','فورم شما موفقانه ثبت ګردید!!');
+      }
+      else{
+        return Redirect::route('getStudent')->with('fail','مشکلی رخ داد لطفا دوباره کوشش نمایید!');
+      }
+    }
   }
 
 
