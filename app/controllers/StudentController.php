12564c6ec061ba1a196ff772fb4d287305e89cd5
Baseer Ahmad Baheer <csd.ku.af@gmail.com>
2014-09-17 11:32:20 +0430

    Few more changes

--- app/controllers/StudentController.php
+++ app/controllers/StudentController.php
@@ -7,25 +7,12 @@ class StudentController extends BaseController {
 
   }
   public function postStudent(){
-
     $validate = Validator::make(Input::all(), array(
-      'q1' => 'required',
-      'q2' => 'required',
-      'q3' => 'required'
+      'pro' => 'required',
+      'pass1' => 'required|min:6',
+      'pass2' => 'required|same:pass1'
     ));
-    if($validate ->fails()){
-      return Redirect::route('postStudent')->withErrors($validate)->withInput();
-    }
-    else{
-      $student = new Student();
-      $student->q1 = Input::get('q1');
-      if($user->save()){
-        return Redirect::route('/user/student')->with('success','فورم شما موفقانه در سیستم ثبت ګردید!');
-      }
-      else{
-        return Redirect::route('/user/student')->with('fail','مشکلی رخ داده لطفا دوباره کوشش نمایید!');
-      }
-    }
+
   }
 
 
